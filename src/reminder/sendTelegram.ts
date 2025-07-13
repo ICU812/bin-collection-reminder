5ce999ec9d8cacd48ad5fdb090122249aff9c42a
Calum Brunt <mulooc@gmail.com>
2025-07-13 15:25:58 +0100

    chore: remove unnecessary fetch import

--- src/reminder/sendTelegram.ts
+++ src/reminder/sendTelegram.ts
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export async function sendTelegramMessage(token: string, chatId: string, message: string): Promise<void> {
   const res = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
     method: "POST",
